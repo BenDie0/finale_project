82dd7f41cde2ae1d488858223554e6c82e166fa7
BenDie0 <58610677+BenDie0@users.noreply.github.com>
2020-03-06 13:35:52 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,376 @@
+namespace SpriteKind {
+    export const projectile3 = SpriteKind.create()
+    export const ammo = SpriteKind.create()
+    export const poop = SpriteKind.create()
+    export const fireball = SpriteKind.create()
+    export const enemy2 = SpriteKind.create()
+}
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+`
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`
+}
+// changes the score
+sprites.onOverlap(SpriteKind.ammo, SpriteKind.poop, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+})
+function hero () {
+    mySprite = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f f f . . . . . 
+. . . . . f f 5 5 5 5 f f . . . 
+. . . . f 5 5 5 5 5 5 5 5 f . . 
+. . . f 5 1 1 1 5 5 1 1 1 5 f . 
+. . . f 5 2 2 1 5 5 1 2 2 5 f . 
+. . f 5 5 1 1 2 5 5 2 1 1 5 5 f 
+. . f 5 5 5 5 5 5 5 5 5 5 5 5 f 
+. . f 5 5 5 5 5 5 5 5 5 5 5 5 f 
+. . f 5 5 5 5 5 5 5 5 5 5 5 5 f 
+. . . f 5 f f f f f f f f 5 f . 
+. . . f 5 5 5 5 5 5 5 5 5 5 f . 
+. . . . f 5 5 5 5 5 5 5 5 f . . 
+. . . . . f f 5 5 5 5 f f . . . 
+. . . . . . . f f f f . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+    mySprite.setPosition(4, 50)
+    controller.moveSprite(mySprite, 0, 100)
+    mySprite.setFlag(SpriteFlag.BounceOnWall, true)
+}
+function cloud () {
+    cloud1 = sprites.createProjectileFromSide(img`
+f f f f . f f f . . f . . . . . f . . . 
+f . . . . f . . f . f . . . . f . f . . 
+f . . . . f . . f . f . . . . f . f . . 
+f f f . . f . f . . f . . . . f f f . . 
+f . . . . f . . f . f . . . f . . . f . 
+f . . . . f . . f . f . . . f . . . f . 
+. . . . . f f f . . f f f . f . . . f . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+`, 0, 175)
+    cloud1.setPosition(150, 110)
+    cloud1.setFlag(SpriteFlag.BounceOnWall, true)
+    cloud1.setKind(SpriteKind.poop)
+}
+function score () {
+	
+}
+function whatever () {
+	
+}
+// what happens when something collides.
+//
+//
+// -->
+//
+//
+//
+//
+//
+//
+// -->
+//
+//
+//
+//
+//
+//
+// -->
+//
+//
+//
+//
+//
+//
+//
+// -->
+sprites.onOverlap(SpriteKind.ammo, SpriteKind.Enemy, function (sprite, otherSprite) {
+    projectile2.destroy()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.enemy2, function (sprite, otherSprite) {
+    game.over(false, effects.melt)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.over(false, effects.melt)
+})
+sprites.onOverlap(SpriteKind.ammo, SpriteKind.enemy2, function (sprite, otherSprite) {
+    PROJECTILE5.destroy()
+})
+function raindrop () {
+	
+}
+function nextlevel () {
+    if (0 == 0) {
+    	
+    }
+}
+function CLOUD_2 () {
+    CLOUD123 = sprites.createProjectileFromSide(img`
+f f f f . f f f . . f . . . . . f . . . 
+f . . . . f . . f . f . . . . f . f . . 
+f . . . . f . . f . f . . . . f . f . . 
+f f f . . f . f . . f . . . . f f f . . 
+f . . . . f . . f . f . . . f . . . f . 
+f . . . . f . . f . f . . . f . . . f . 
+. . . . . f f f . . f f f . f . . . f . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+`, 0, -150)
+    CLOUD123.setPosition(150, 7)
+    CLOUD123.setFlag(SpriteFlag.BounceOnWall, true)
+    CLOUD123.setKind(SpriteKind.poop)
+}
+function ball () {
+	
+}
+let projectile: Sprite = null
+let CLOUD123: Sprite = null
+let PROJECTILE5: Sprite = null
+let projectile2: Sprite = null
+let cloud1: Sprite = null
+let mySprite: Sprite = null
+CLOUD_2()
+whatever()
+ball()
+raindrop()
+hero()
+cloud()
+game.splash("survive as long", "as possible")
+nextlevel()
+// shoots a projectile from the sprite
+game.onUpdateInterval(500, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f f . . . . . 
+f . . . f f 8 1 1 1 1 f f f . . 
+4 f . f 1 8 1 8 2 2 2 2 f 4 f . 
+4 4 f f 8 1 8 1 1 1 1 1 f 4 f . 
+4 4 4 f 1 8 1 8 2 2 2 2 f 4 4 f 
+f f 4 f 1 1 1 1 1 1 1 1 f 4 4 f 
+4 4 4 f 2 2 2 2 2 2 2 2 f 4 4 f 
+4 4 f f 1 1 1 1 1 1 1 1 f 4 f . 
+4 f . f 2 2 2 2 2 2 2 2 f 4 f . 
+f . . . f f 1 1 1 1 1 f f f . . 
+. . . . . . f f f f f . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, mySprite, 100, 0)
+    projectile.setKind(SpriteKind.ammo)
+})
+// this is the projectiles from the sky.
+//
+//
+//
+// -->
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// -->
+game.onUpdateInterval(500, function () {
+    projectile2 = sprites.createProjectileFromSprite(img`
+. . . . . 5 5 5 5 5 5 . . . . . 
+. . . 5 5 5 5 5 5 5 5 5 5 . . . 
+. . 5 5 5 5 4 4 4 4 4 4 5 5 . . 
+. 5 5 5 4 4 2 2 4 4 4 4 4 5 5 . 
+. 5 5 4 4 4 2 5 2 2 4 5 4 4 5 . 
+5 5 5 4 5 2 2 4 2 2 4 2 4 4 5 5 
+5 5 4 4 2 2 2 2 2 2 2 2 2 4 5 5 
+5 5 4 4 4 2 2 4 2 2 4 2 2 5 4 5 
+5 5 4 4 2 2 2 2 2 2 2 2 4 5 4 5 
+5 5 4 5 4 2 2 2 2 2 2 4 4 4 4 5 
+5 5 5 4 5 4 2 2 4 4 2 2 4 4 4 5 
+. 5 5 4 4 4 4 5 4 4 4 4 4 4 5 . 
+. 5 5 5 4 4 5 4 4 5 5 4 4 5 5 . 
+. . 5 5 5 4 4 4 4 4 4 5 5 5 . . 
+. . . 5 5 5 5 5 5 5 5 5 5 . . . 
+. . . . . 5 5 5 5 5 5 . . . . . 
+`, cloud1, -50, 0)
+    projectile2.setKind(SpriteKind.Enemy)
+    projectile2.y += 0
+    projectile2.vx += 0
+})
+game.onUpdateInterval(500, function () {
+    PROJECTILE5 = sprites.createProjectileFromSprite(img`
+. . . . . 5 5 5 5 5 5 . . . . . 
+. . . 5 5 5 5 5 5 5 5 5 5 . . . 
+. . 5 5 5 5 4 4 4 4 4 4 5 5 . . 
+. 5 5 5 4 4 2 2 4 4 4 4 4 5 5 . 
+. 5 5 4 4 4 2 5 2 2 4 5 4 4 5 . 
+5 5 5 4 5 2 2 4 2 2 4 2 4 4 5 5 
+5 5 4 4 2 2 2 2 2 2 2 2 2 4 5 5 
+5 5 4 4 4 2 2 4 2 2 4 2 2 5 4 5 
+5 5 4 4 2 2 2 2 2 2 2 2 4 5 4 5 
+5 5 4 5 4 2 2 2 2 2 2 4 4 4 4 5 
+5 5 5 4 5 4 2 2 4 4 2 2 4 4 4 5 
+. 5 5 4 4 4 4 5 4 4 4 4 4 4 5 . 
+. 5 5 5 4 4 5 4 4 5 5 4 4 5 5 . 
+. . 5 5 5 4 4 4 4 4 4 5 5 5 . . 
+. . . 5 5 5 5 5 5 5 5 5 5 . . . 
+. . . . . 5 5 5 5 5 5 . . . . . 
+`, CLOUD123, -50, 0)
+    PROJECTILE5.setKind(SpriteKind.enemy2)
+    PROJECTILE5.y += 0
+    PROJECTILE5.vx += 0
+})
